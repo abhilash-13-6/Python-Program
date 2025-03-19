739886727689d4815c14c6901d7cfbfff62de93e
Abhilash S Devadiga <abhilashsdevadiga@gmail.com>
2025-03-19 14:26:55 +0530

    Add files via upload

---
+++ oddoreven.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number: "))
+
+# Check whether the number is odd or even
+if number % 2 == 0:
+    print(f"The number {number} is even.")
+else:
+    print(f"The number {number} is odd.")

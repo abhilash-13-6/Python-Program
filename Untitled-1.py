d008cba21ef92d0b70cab0ad53e985752c1b77fd
Abhilash S Devadiga <abhilashsdevadiga@gmail.com>
2025-03-19 15:02:35 +0530

    Delete Untitled-1.py

--- Untitled-1.py
+++
@@ -1,8 +0,0 @@
-n1 = int(input("enter start number:"))
-n2 = int(input("enter last number:"))
-n=int(input("enter n:"))
-def range_u():
-    for i in range(n1,n2+1):
-        if n==i:
-            print(i,"is in range")
-            

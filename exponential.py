bde80ec439ca208a79507f45a4712776265098bc
Abhilash S Devadiga <abhilashsdevadiga@gmail.com>
2025-08-01 15:22:17 +0530

    Create exponential.py

---
+++ exponential.py
@@ -0,0 +1,19 @@
+import time
+
+def exponential_time(n):
+    
+    print(f"\nO(2^n) for n={n}")
+    start = time.time()
+    def helper(k):
+        if k == 0:
+            return
+        helper(k - 1)
+        helper(k - 1)
+    helper(n)
+    end = time.time()
+    print(f"Time taken: {end - start:.6f} seconds")
+
+
+exponential_time(10)  
+exponential_time(15) 
+exponential_time(30)  
